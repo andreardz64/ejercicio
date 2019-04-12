fe23d1f2a7c41ef35be3e2387f21399a3dcc549e
Andrea <jmeza64@hote>
2019-04-12 18:56:29 -0500

    save & load

---
+++ TileMap/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Transform player;
+    public string tempJson = "";
+
+
+   public void Save()
+    {
+        PlayerData data = new PlayerData();
+        data.position = player.position;
+        data.life = 75;
+        string dataString = JsonUtility.ToJson(data);
+        tempJson = dataString;
+        Debug.Log("Saved");
+        Debug.Log("Data String: " + dataString);
+    }
+
+    public void Load()
+    {
+        string dataString = tempJson;
+        PlayerData data = JsonUtility.FromJson<PlayerData>(dataString);
+        player.position = data.position;
+
+    }
+
+
+}
+
+public class PlayerData
+{
+    public Vector3 position;
+    public int life;
+}
--- TileMap/Assets/Scripts/controller.cs
+++ TileMap/Assets/Scripts/controller.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class controller : MonoBehaviour
 {
+    public float speed;
+
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+       
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f,0);
+
+        rb.AddForce(movement * speed);
     }
 }
